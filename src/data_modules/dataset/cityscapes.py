8c7eca7326a3c8bd304b3bbf2a92dae895b31546
Mradul Agrawal <theroyalpekka@gmail.com>
2021-09-15 20:42:50 +0530

    Baseline

--- src/data_modules/dataset/cityscapes.py
+++ src/data_modules/dataset/cityscapes.py
@@ -1,3 +1,5 @@
+from torch.utils.data import Dataset
+
 class CityScapes(Dataset):
 
   def __init__(self, image, label, transforms):
@@ -12,49 +14,6 @@ class CityScapes(Dataset):
 
     if self.transform is not None:
         image = self.transform(image)
-
-    return image, label
-
-  def __len__(self):
-
-    return len(self.data)
-
-
-class CityScapesCutMix(Dataset):
-
-  def __init__(self, image, label, transforms, num_mix=1, beta=1.0, prob=1.0):
-
-    self.data = image
-    self.label = label
-    self.transform = transforms
-    self.num_mix = num_mix
-    self.beta = beta
-    self.prob = prob
-
-  def __getitem__(self, index):
-    image = (self.data[index]).copy()
-    label = (self.label[index]).copy()
-
-    for _ in range(self.num_mix):
-        r = np.random.rand(1)
-        if self.beta <= 0 or r > self.prob:
-            continue
-
-        lam = np.random.beta(self.beta, self.beta)
-        rand_index = random.choice(range(len(self)))
-
-
-        image2 = self.data[rand_index]
-        label2 = self.label[rand_index]
-
-        bbx1, bby1, bbx2, bby2 = rand_bbox(image.shape, lam)
-
-        image[bby1:bby2, bbx1:bbx2, :] = image2[bby1:bby2, bbx1:bbx2, :]
-        label[bby1:bby2, bbx1:bbx2] = label2[bby1:bby2, bbx1:bbx2]
-
-
-    if self.transform is not None:
-       image = self.transform(image)
 
     return image, label
 
