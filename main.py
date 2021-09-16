73a51683ffc4fa768845d0d4768f7cef3065e667
Mradul Agrawal <theroyalpekka@gmail.com>
2021-09-16 22:17:32 +0530

    Baseline

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import src.utils.config as config
 
 def main():
 	arg_parse = argparse.ArgumentParser(description="")
-    arg_parser.add_argument(
-        'config',
-        metavar='config_json_file',
-        default='None',
-        help='The Configuration file in json format')
-    args = arg_parser.parse_args()
+	arg_parser.add_argument(
+		'config',
+		metavar='config_json_file',
+		default='None',
+		help='The Configuration file in json format')
+	args = arg_parser.parse_args()
 
-    config = config.process_config(args.config)
+	config = config.process_config(args.config)
 
 
 
