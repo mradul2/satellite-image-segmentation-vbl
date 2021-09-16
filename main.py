b1835be9c098cdd43c2f402178450db44b5e7a00
Mradul Agrawal <theroyalpekka@gmail.com>
2021-09-16 22:22:33 +0530

    Baseline

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+"""
+__author__ = "Hager Rady and Mo'men AbdelRazek"
+
+Main
+-Capture the config file
+-Process the json config passed
+-Create an agent instance
+-Run the agent
+"""
+
 import argparse
-import src.utils.config as config
+from utils.config import *
+
+from agents import *
+
 
 def main():
-	arg_parse = argparse.ArgumentParser(description="")
-	arg_parser.add_argument(
-		'config',
-		metavar='config_json_file',
-		default='None',
-		help='The Configuration file in json format')
-	args = arg_parser.parse_args()
+    # parse the path of the json config file
+    arg_parser = argparse.ArgumentParser(description="")
+    arg_parser.add_argument(
+        'config',
+        metavar='config_json_file',
+        default='None',
+        help='The Configuration file in json format')
+    args = arg_parser.parse_args()
 
-	config = config.process_config(args.config)
+    # parse the config json file
+    config = process_config(args.config)
 
+    # Create the Agent and pass all the configuration to it then run it..
+    agent_class = globals()[config.agent]
+    agent = agent_class(config)
+    agent.run()
+    agent.finalize()
 
 
 if __name__ == '__main__':
-	main()
+    main()
