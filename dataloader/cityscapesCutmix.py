d1c93b83cb852946d49b52cb162195ded64a3dfd
Mradul Agrawal <theroyalpekka@gmail.com>
2021-09-18 13:39:52 +0530

    Baseline

--- dataloader/cityscapesCutmix.py
+++ dataloader/cityscapesCutmix.py
@@ -10,38 +10,6 @@ import torchvision.transforms as transforms
 from utils.scripts import CityLabels
 
 class CityScapes(Dataset):
-
-    def encode(self, mask):
-        res = np.zeros_like(mask)
-        for label in CityLabels:
-            res[mask == label.id] = label.trainId
-            return res
-
-    def rand_bbox(self, size, lam):
-        if len(size) == 4:
-            W = size[1]
-            H = size[0]
-        elif len(size) == 3:
-            W = size[1]
-            H = size[0]
-        else:
-            raise Exception
-
-        cut_rat = np.sqrt(1. - lam)
-        cut_w = np.int(W * cut_rat)
-        cut_h = np.int(H * cut_rat)
-
-        # uniform
-        cx = np.random.randint(W)
-        cy = np.random.randint(H)
-
-        bbx1 = np.clip(cx - cut_w // 2, 0, W)
-        bby1 = np.clip(cy - cut_h // 2, 0, H)
-        bbx2 = np.clip(cx + cut_w // 2, 0, W)
-        bby2 = np.clip(cy + cut_h // 2, 0, H)
-
-    return bbx1, bby1, bbx2, bby2
-    
     def __init__(self, image_path, label_path, transforms, num_mix=1, beta=1.0, prob=1.0):
         self.data_path = image_path
         self.label_path = label_path
@@ -90,6 +58,36 @@ class CityScapes(Dataset):
     def __len__(self):
         return len(self.data)
 
+    def encode(self, mask):
+        res = np.zeros_like(mask)
+        for label in CityLabels:
+            res[mask == label.id] = label.trainId
+            return res
+
+    def rand_bbox(self, size, lam):
+        if len(size) == 4:
+            W = size[1]
+            H = size[0]
+        elif len(size) == 3:
+            W = size[1]
+            H = size[0]
+        else:
+            raise Exception
+
+        cut_rat = np.sqrt(1. - lam)
+        cut_w = np.int(W * cut_rat)
+        cut_h = np.int(H * cut_rat)
+
+        # uniform
+        cx = np.random.randint(W)
+        cy = np.random.randint(H)
+
+        bbx1 = np.clip(cx - cut_w // 2, 0, W)
+        bby1 = np.clip(cy - cut_h // 2, 0, H)
+        bbx2 = np.clip(cx + cut_w // 2, 0, W)
+        bby2 = np.clip(cy + cut_h // 2, 0, H)
+
+    return bbx1, bby1, bbx2, bby2
 
 
 class CityScapesDataLoader:
